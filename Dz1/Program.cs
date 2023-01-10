8b8c8470cc59ee254b7fdb2b6de78b8ebd28b90b
Aleksandr <aleksandr.evlanicov@gmail.com>
2023-01-10 16:54:22 +0300

    Dz1

---
+++ Dz1/Program.cs
@@ -0,0 +1,43 @@
+﻿int [,] array = new int [3,4] {
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}  
+};
+
+for(int i = 0; i < array.GetLength(0); i++)
+
+{
+    for( int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i,j] + " ");
+        
+    }
+    Console.WriteLine();
+}
+
+  for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = array.GetLength(1) - 1; j > 0; j--)
+                {
+
+                    for (int k = 0; k < j; k++)
+                    {
+                        if (array[i, k] < array[i, k + 1])
+                        {
+                            int temp = array[i, k];
+                            array[i, k] = array[i, k + 1];
+                            array[i, k + 1] = temp;
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j]+ " " );
+                }
+                Console.WriteLine();
+            }
